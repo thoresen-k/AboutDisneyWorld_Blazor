888163640dd8e8f7817819152452b8f67ccd4ce5
Kelli Thoresen <kellithoresen@gmail.com>
2024-05-21 03:42:38 -0700

    Add project files.

---
+++ AboutDisneyWorld_Blazor/AboutDisneyWorldConstants.cs
@@ -0,0 +1,14 @@
+﻿namespace AboutDisneyWorld_Blazor
+{
+    namespace AboutDisneyWorldConstants
+    {
+        public class AboutDisneyWorldConstants
+        {
+            public const string MainWindowHeader = "Explore Disney World";
+            public const string MainWindowDescription = "Disney World is home to four theme parks, two water parks, and Saratoga Springs";
+
+            public const string AnimalKingdomHeader = "Animal Kingdom Park";
+            public const string AnimalKingdomDescription = "Animal Kingdom Park is for the animal lovers";
+        }
+    }
+}
