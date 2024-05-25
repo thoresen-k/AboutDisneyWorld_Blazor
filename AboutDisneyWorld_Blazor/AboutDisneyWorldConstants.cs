092007f87fc60b41d2439002bbe8f573042ec196
Kelli Thoresen <kellithoresen@gmail.com>
2024-05-25 05:02:59 -0700

    create theme park pages

--- AboutDisneyWorld_Blazor/AboutDisneyWorldConstants.cs
+++ AboutDisneyWorld_Blazor/AboutDisneyWorldConstants.cs
@@ -7,8 +7,17 @@
             public const string MainWindowHeader = "Explore Disney World";
             public const string MainWindowDescription = "Disney World is home to four theme parks, two water parks, and Saratoga Springs";
 
-            public const string AnimalKingdomHeader = "Animal Kingdom Park";
+            public const string AnimalKingdomHeader = "Animal Kingdom";
             public const string AnimalKingdomDescription = "Animal Kingdom Park is for the animal lovers";
+
+            public const string EpcotHeader = "Epcot";
+            public const string EpcotDescription = "Eat and drink your way around the world";
+
+            public const string HollywoodStudiosHeader = "Hollywood Studios";
+            public const string HollywoodStudiosDescription = "Walk down Main Street and pretend you're a movie star";
+
+            public const string MagicKingdomHeader = "Magic Kingdom";
+            public const string MagicKingdomDescription = "Heal your inner child and get transported to the most magical place on earth";
         }
     }
 }
