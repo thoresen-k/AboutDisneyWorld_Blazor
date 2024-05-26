3032e54825f22959b5e77faef33f44898421a53d
Kelli Thoresen <kellithoresen@gmail.com>
2024-05-25 22:29:24 -0700

    * add in saratoga springs page * rename description to tagline

--- AboutDisneyWorld_Blazor/AboutDisneyWorldConstants.cs
+++ AboutDisneyWorld_Blazor/AboutDisneyWorldConstants.cs
@@ -5,19 +5,22 @@
         public class AboutDisneyWorldConstants
         {
             public const string MainWindowHeader = "Explore Disney World";
-            public const string MainWindowDescription = "Disney World is home to four theme parks, two water parks, and Saratoga Springs";
+            public const string MainWindowTagline = "Disney World is home to four theme parks, two water parks, and Saratoga Springs";
 
             public const string AnimalKingdomHeader = "Animal Kingdom";
-            public const string AnimalKingdomDescription = "Animal Kingdom Park is for the animal lovers";
+            public const string AnimalKingdomTagline = "Animal Kingdom Park is for the animal lovers";
 
             public const string EpcotHeader = "Epcot";
-            public const string EpcotDescription = "Eat and drink your way around the world";
+            public const string EpcotTagline = "Eat and drink your way around the world";
 
             public const string HollywoodStudiosHeader = "Hollywood Studios";
-            public const string HollywoodStudiosDescription = "Walk down Main Street and pretend you're a movie star";
+            public const string HollywoodStudiosTagline = "Walk down Main Street and pretend you're a movie star";
 
             public const string MagicKingdomHeader = "Magic Kingdom";
-            public const string MagicKingdomDescription = "Heal your inner child and get transported to the most magical place on earth";
+            public const string MagicKingdomTagline = "Heal your inner child and get transported to the most magical place on earth";
+
+            public const string SaratogaSpringsHeader = "Saratoga Springs";
+            public const string SaratogaSpringsTagline = "Shop to your legs drop as well as enjoy fine dining and entertainment";
         }
     }
 }
