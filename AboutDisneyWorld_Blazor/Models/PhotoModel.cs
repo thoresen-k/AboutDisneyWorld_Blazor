d0837fa41220caa43d8b5aa4738b609fa0a709af
Kelli Thoresen <kellithoresen@gmail.com>
2025-04-20 17:32:07 -0700

    photo model

---
+++ AboutDisneyWorld_Blazor/Models/PhotoModel.cs
@@ -0,0 +1,13 @@
+﻿namespace AboutDisneyWorld_Blazor.Models
+{
+    public class PhotoModel(string href, string title, string caption, DateTime date)
+    {
+        public string Href => href;
+
+        public string Title => title;
+
+        public string Caption => caption;
+
+        public DateTime Date => date;
+    }
+}
